d6b181fcba4c5e5d33ad204596756bdbfcc19dab
Yexi Jiang <yexijiang@gmail.com>
2012-10-31 18:15:24 -0400

    work on collector minimal functionality

---
+++ system-event-processing/src/collector/collector.cc
@@ -0,0 +1,143 @@
+/*
+ * collector.cc
+ *
+ *  Created on: Oct 31, 2012
+ *      Author: yexijiang
+ */
+
+#include "collector.h"
+
+namespace event
+{
+using namespace std;
+
+int Collector::dataServicePort_ = 32168;    //  default port number to receive data
+bool Collector::dataServiceStop_ = false;    //  data service is running by default
+pthread_rwlock_t Collector::stopSymbolrwlock_;
+
+Collector::Collector(vector<string> vecPeerCollectorIPs, int communicationPort, int dataPort)
+{
+  dataServicePort_ = dataPort;
+
+  pthread_rwlock_init(&stopSymbolrwlock_, NULL);
+}
+
+Collector::~Collector()
+{
+  pthread_rwlock_destroy(&stopSymbolrwlock_);
+}
+
+void Collector::Run()
+{
+  pthread_t dataServicePid;
+  pthread_create(&dataServicePid, NULL, _DataReceiveService, NULL);
+
+  pthread_join(dataServicePid, NULL);
+}
+
+void *Collector::_DataReceiveService(void *arg)
+{
+  struct sockaddr_in dataRecieveServiceAddr; // Server Internet address
+  //  initialize server address
+  bzero(&dataRecieveServiceAddr, sizeof(dataRecieveServiceAddr));
+  dataRecieveServiceAddr.sin_family = AF_INET;
+  dataRecieveServiceAddr.sin_addr.s_addr = htons(INADDR_ANY);
+  dataRecieveServiceAddr.sin_port = htons(dataServicePort_);
+
+  int dataReceiveServerSocketFd = socket(AF_INET, SOCK_STREAM, 0);
+  if (dataReceiveServerSocketFd < 0)
+  {
+    fprintf(stderr, "[%s] Collector data receive service creates socket failed.\n", GetCurrentTime().c_str());
+    exit(1);
+  }
+  else
+    fprintf(stdout, "[%s] Collector data receive service socket created...\n", GetCurrentTime().c_str());
+
+  //  bind socket and address
+  if (bind(dataReceiveServerSocketFd, (struct sockaddr*) &dataRecieveServiceAddr, sizeof(dataRecieveServiceAddr)))
+  {
+    fprintf(stderr, "[%s] Collector data receive service bind port: %d failed.\n", GetCurrentTime().c_str(), dataServicePort_);
+    close(dataReceiveServerSocketFd);
+    exit(1);
+  }
+  else
+    fprintf(stdout, "[%s] Collector data receive service port binded to port %d...\n", GetCurrentTime().c_str(), dataServicePort_);
+
+  //  listen
+  if (listen(dataReceiveServerSocketFd, 500))
+  {
+    fprintf(stderr, "[%s] Collector data receive service listen failed.\n", GetCurrentTime().c_str());
+    close(dataReceiveServerSocketFd);
+    exit(1);
+  }
+  else
+    fprintf(stdout, "[%s] Collector data receive service listening on port %d...\n", GetCurrentTime().c_str(), dataServicePort_);
+
+  while (true)
+  {
+    int connectionSockedFd = accept(dataReceiveServerSocketFd, NULL, 0);
+
+    //  create worker to receive data
+    pthread_t dataReceiveWorkerPid;
+    pthread_create(&dataReceiveWorkerPid, NULL, _DataReceiveWorker, (void *)&connectionSockedFd);
+
+    close(connectionSockedFd);
+  }
+
+  close(dataReceiveServerSocketFd);
+  return NULL;
+}
+
+void *Collector::_DataReceiveWorker(void *arg)
+{
+  int *socketFd = (int *)arg;
+  char buffer[1024];
+  stringstream ss;
+
+  while(recv(*socketFd, buffer, 1024, 0) > 0)
+  {
+    ss << buffer;
+  }
+  cout << ss.str() << endl;
+
+  return NULL;
+}
+
+}
+
+int main(int argc, char *argv[])
+{
+  using namespace std;
+  using namespace event;
+
+  int commandPort = 32167;
+  int dataPort = 32168;
+
+  if(argc < 2)
+  {
+    printf("\nusage: collector ips [command-port] [collector-data-port]\n");
+    printf("Options:\n");
+    printf("\tips\t\t\t\tList of IPs of all the collectors, include the server itself, separated by ','.\n");
+    printf("\tcommand-port\t\t\tPort number of command service. Default is 32100.\n");
+    printf("\tcollector-data-port\t\tData port of remote collectors. Default is 32168.\n");
+    exit(1);
+  }
+
+  vector<string> vecIPs;
+  if(argc >= 2)
+  {
+    string ipStr(argv[1]);
+    Split(ipStr, ',', vecIPs, true);
+  }
+
+  if(argc >= 3)
+    commandPort = atoi(argv[2]);
+
+  if(argc >= 4)
+    dataPort = atoi(argv[3]);
+
+  Collector collector(vecIPs, commandPort, dataPort);
+  collector.Run();
+
+  return 0;
+}
--- system-event-processing/src/collector/collector.h
+++ system-event-processing/src/collector/collector.h
@@ -8,14 +8,40 @@
 #ifndef COLLECTOR_H_
 #define COLLECTOR_H_
 
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <pthread.h>
 #include <boost/shared_ptr.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include "../common/utility.pb.h"
 #include "../common/common.h"
 
 namespace event
 {
 
+class Collector
+{
+public:
+  Collector(std::vector<std::string> vecPeerCollectorIPs, int communicationPort, int dataPort);
+  ~Collector();
+
+  void Run();
+
+protected:
+  static void *_DataReceiveService(void *);
+  static void *_DataReceiveWorker(void *);
+
+private:
+  static int dataServicePort_;
+  static bool dataServiceStop_;
+  static pthread_rwlock_t stopSymbolrwlock_;
+
+};
 
 }
 
